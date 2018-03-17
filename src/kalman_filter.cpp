01d03f92590ddab51c79dbebcf3d8cc1e48a41e0
Moises Vargas M <moisesweb@gmail.com>
2018-03-17 13:46:12 -0500

    Add logger to track process information

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -6,9 +6,18 @@ using Eigen::VectorXd;
 // Please note that the Eigen library does not initialize 
 // VectorXd or MatrixXd objects with zeros upon creation.
 
-KalmanFilter::KalmanFilter() {}
+KalmanFilter::KalmanFilter() {
+  debug_option = true;
+  if(debug_option) {
+    //Set initial headers
+    log.log("metric_type,sensor_type,px,py,vx,vy,ro,phi,ro_dot");
+  }
 
-KalmanFilter::~KalmanFilter() {}
+}
+
+KalmanFilter::~KalmanFilter() {
+  log.close();
+}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
@@ -23,6 +32,14 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 void KalmanFilter::Predict() {
   x_ = F_ * x_;
   P_ = F_ * P_ * F_.transpose() + Q_;
+
+  if(debug_option) {
+    string px = to_string(x_(0));
+    string py = to_string(x_(1));
+    string vx = to_string(x_(2));
+    string vy = to_string(x_(3));
+    log.log("prediction,none,"+ px +","+ py +","+ vx +","+ vy +",0,0,0");
+  }
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -58,4 +75,16 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   uint x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
+
+  if(debug_option) {
+    string ro = to_string(xr_(0));
+    string phi = to_string(xr_(1));
+    string ro_dot = to_string(xr_(2));
+    string log_line = "update,lidar," +
+                      to_string(px) +","+ to_string(py) +
+                      ","+ to_string(vx) +","+ to_string(vy) +
+                      ","+ ro +","+ phi +","+ ro_dot;
+    log.log(log_line);
+  }
+
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -1,6 +1,7 @@
 #ifndef KALMAN_FILTER_H_
 #define KALMAN_FILTER_H_
 #include "Eigen/Dense"
+#include "logger.h"
 
 class KalmanFilter {
 public:
@@ -63,7 +64,9 @@ public:
    * @param z The measurement at k+1
    */
   void UpdateEKF(const Eigen::VectorXd &z);
-
+private:
+  bool debug_option;
+  Logger log;
 };
 
 #endif /* KALMAN_FILTER_H_ */
