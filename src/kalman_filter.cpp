569db5c2f6993b43275a5be0d04a5b6bcb029539
Moises Vargas M <moisesweb@gmail.com>
2018-03-17 10:10:52 -0500

    Implement update measurement for Radar and Lidar
    * For Radar UpdateEKF as extended version of Kalman Filter
      requires to covert from Cartesian to Polar coordinates
      This will be applied to Radar metrics

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -21,22 +21,41 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K = P_ * Ht * S.inverse();
+
+  x_ = x_ + (K * y);
+  uint x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  VectorXd xr_(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  xr_ << sqrt(px*px + py*py),
+         atan2(py,px),
+         (px*vx + py*vy)/sqrt(px*px + py*py);
+
+  VectorXd y = z - xr_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K = P_ * Ht * S.inverse();
+
+  x_ = x_ + (K * y);
+  uint x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
